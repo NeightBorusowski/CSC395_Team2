03e60da860a8d4119b1b13a5f5112278db503f79
TylerDerk2004 <derkovit@canisius.edu>
2024-09-19 20:52:41 -0400

    added comments

--- Flask_Server/app.py
+++ Flask_Server/app.py
@@ -24,6 +24,10 @@ def submit_data():
     return jsonify({'status': 'success', 'message': 'Data received successfully!'})
 
 #add function to send data to llm and receive back response
+#In this function, im creating a request to send to ollama.py (formatting a question with the variables to
+#send too ollama.py)
+
+
 
 #add function to send data returned from llm to frontend
 
