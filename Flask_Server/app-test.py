5a5bdebe0bdf83e292e574a3e7c0b8126598ce4f
ConnorRut <simonbluebones@gmail.com>
2024-09-24 18:05:07 -0400

    small test file edits #7

--- Flask_Server/app-test.py
+++ Flask_Server/app-test.py
@@ -1,10 +1,13 @@
 import unittest
-
+from unittest.mock import patch, MagicMock
+from flask import Flask, jsonify
+import json
+from app import app, create_question, generate_ollama_response
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def t(self):
         self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
-    unittest.main(
+    unittest.main()
